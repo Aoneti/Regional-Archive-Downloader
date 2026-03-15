bf590287658b183266150d374e1b5a16e3c583c4
Arseny <dybowit@gmail.com>
2026-03-15 19:26:47 +0300

    Update content-script.js

--- content-script.js
+++ content-script.js
@@ -102,7 +102,9 @@ function sanitizeForFilename(s) {
 }
 
 function imageUrl(unit, page) {
-  return `https://af.yar-archives.ru/archive27/image/${unit}?n=${page}`;
+  const base = location.origin;
+  const archiveNum = location.pathname.match(/\/archive(\d+)\//)?.[1] ?? '27';
+  return `${base}/archive${archiveNum}/image/${unit}?n=${page}`;
 }
 
 function pad(num, w) {
